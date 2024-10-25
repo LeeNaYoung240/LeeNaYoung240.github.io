f986821e6d4558cfc8d72b8b09fc568a0c2f92f4
LeeNaYoung240 <ajd092710@gmail.com>
2024-10-25 20:40:11 +0900

    fix : 원상복구

--- _javascript/home.js
+++ _javascript/home.js
@@ -1,8 +1,9 @@
 import { basic, initSidebar, initTopbar } from './modules/layouts';
 import { initLocaleDatetime, loadImg } from './modules/plugins';
 
-loadImg();
-initLocaleDatetime();
+basic();
 initSidebar();
 initTopbar();
-basic();
+initLocaleDatetime();
+loadImg();
+
